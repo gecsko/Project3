d485e68bde4d1c0c4ea13952a5c3f2eda42f4566
gecsko <gecsko@gmail.com>
2015-12-07 01:37:13 +0800

    Test

--- Alexandria_Code/alexandria/app/src/main/java/it/jaschke/alexandria/About.java
+++ Alexandria_Code/alexandria/app/src/main/java/it/jaschke/alexandria/About.java
@@ -14,6 +14,7 @@ public class About extends Fragment {
 
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
