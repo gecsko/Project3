efdb07e7cd290e5c0ef3f1cb305db22e61119cdd
gecsko <gecsko@gmail.com>
2015-12-09 01:18:41 +0800

    test

--- Football_Scores-Starting/Football_Scores-master/app/src/main/java/barqsoft/footballscores/NowWidgetIntentService.java
+++ Football_Scores-Starting/Football_Scores-master/app/src/main/java/barqsoft/footballscores/NowWidgetIntentService.java
@@ -12,9 +12,7 @@ import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.widget.RemoteViews;
 
-/**
- * Created by Tim on 2015/12/8.
- */
+
 public class NowWidgetIntentService extends IntentService {
         public NowWidgetIntentService() {
             super("NowWidgetIntentService");
@@ -30,18 +28,14 @@ public class NowWidgetIntentService extends IntentService {
             // Perform this loop procedure for each Today widget
             for (int appWidgetId : appWidgetIds) {
                 // Find the correct layout based on the widget's width
-                int widgetWidth = getWidgetWidth(appWidgetManager, appWidgetId);
-
-                layoutId = R.layout.widget_now_small;
-
+                int layoutId = R.layout.widget_now_small;
                 RemoteViews views = new RemoteViews(getPackageName(), layoutId);
 
-                // Add the data to the RemoteViews
-                views.setImageViewResource(R.id.widget_icon, weatherArtResourceId);
-                // Content Descriptions for RemoteViews were only added in ICS MR1
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
-                    setRemoteContentDescription(views, description);
-                }
+                views.setTextViewText(R.id.widget_high_temperature, "Test");
+
+               /// if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                //    setRemoteContentDescription(views, description);
+                //}
 
                 // Create an Intent to launch MainActivity
                 Intent launchIntent = new Intent(this, MainActivity.class);
@@ -53,32 +47,4 @@ public class NowWidgetIntentService extends IntentService {
             }
         }
 
-        private int getWidgetWidth(AppWidgetManager appWidgetManager, int appWidgetId) {
-            // Prior to Jelly Bean, widgets were always their default size
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-                return getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
-            }
-            // For Jelly Bean and higher devices, widgets can be resized - the current size can be
-            // retrieved from the newly added App Widget Options
-            return getWidgetWidthFromOptions(appWidgetManager, appWidgetId);
-        }
-
-        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-        private int getWidgetWidthFromOptions(AppWidgetManager appWidgetManager, int appWidgetId) {
-            Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
-            if (options.containsKey(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)) {
-                int minWidthDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
-                // The width returned is in dp, but we'll convert it to pixels to match the other widths
-                DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-                return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, minWidthDp,
-                        displayMetrics);
-            }
-            return  getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
-        }
-
-        @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
-        private void setRemoteContentDescription(RemoteViews views, String description) {
-            views.setContentDescription(R.id.widget_icon, description);
-        }
-    }
 }
